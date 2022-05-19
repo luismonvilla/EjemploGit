0dd985e15260a3dca2df4a5cc9d136e526b3f502
luismonvilla <86098055+luismonvilla@users.noreply.github.com>
2022-05-19 14:33:15 -0600

    Delete EjemploGitHub.java

--- EjemploGitHub.java
+++
@@ -1,7 +0,0 @@
-
-public class EjemploGitHub {
-    public static void main (String args []) {
-        System.out.println(" Este es un ejemplo programado, donde se crea un repositorio para la plataforma de GitHub.");
-    }
-    }
-            
