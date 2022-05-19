41f58d70509d274a3717bf2bafd53f4dbdc26cc6
luismonvilla <86098055+luismonvilla@users.noreply.github.com>
2022-05-19 14:36:48 -0600

    Add files via upload

---
+++ EjemploGitHub.java
@@ -0,0 +1,7 @@
+
+public class EjemploGitHub {
+    public static void main (String args []) {
+        System.out.println(" Este es un ejemplo programado, donde se crea un repositorio para la plataforma de GitHub.");
+    }
+    }
+            
